1e4b3d6a27a80345c2a73bebc4bb9e1f53115b61
hark130 <hark130@yahoo.com>
2017-08-20 16:13:25 -0500

    COLOR: Implemented Main Armor Palette
    ColorPalette class successfully instantiated as MainArmorPalette.  Currently, only Monochromatic - Primary Color color scheme has been implemented in the parent class of ColorPalette.  rando_color_scheme() is hard-coded to return listOfColorSchemes[0] so don't forget to modify rando_color_scheme() after new color schemes are implemented.  In fact, add a list of things to update once new color schemes are updated. -Hark

--- Harklepalette.py
+++ Harklepalette.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Color:
 	'This class defines the attributes of one color'
 	
@@ -148,6 +150,267 @@ class Color:
 
 		return retVal
 
+
+class ColorPalette:
+	'This class can randomly choose complimentary colors from a collection based on established color schemes'
+
+	listOfColors = []  # This will hold all the colors available in this specific palette
+	validColors = [ \
+		"Greyscale", "Red", "Red Orange", \
+		"Orange", "Orange Yellow", "Yellow", \
+		"Yellow Green", "Green", "Green Blue", \
+		"Blue", "Blue Indigo", "Indigo", \
+		"Indigo Violet", "Violet", "Violet Red", \
+	]
+	validTypes = [ \
+		"Primary", "Secondary", "Tertiary", \
+	]
+	validSchemes = [ \
+		"Monochromatic - Primary", "Monochromatic - Secondary", "Monochromatic - Tertiary", \
+		"2 Colors - Analogous", "2 Colors - Complementary", "3 Colors - Triad", \
+		"3 Colors - Split Complementary", "3 Colors - Secondary", "Random Chaos", \
+		"Earthy", "Urban", "Emo", \
+	]
+	implementedSchemes = [ \
+		"Monochromatic - Primary", \
+	]
+	listOfPrimaryColors = [ \
+		"Red", "Yellow", "Blue", \
+	]
+	scheme = ""
+
+	def __init__(self, scheme):
+		### INPUT VALIDATION ###
+		if not isinstance(scheme, str):
+			raise TypeError("Color scheme is not an string")
+		elif scheme not in validSchemes:
+			raise ValueError("Invalid color scheme")
+		elif scheme not in implementedSchemes:
+			raise RuntimeError("This valid color scheme has not yet been implemented")
+
+		### INITIALIZAITON ###
+		self.scheme = scheme
+
+
+	def count_colors(self, wheelColorToCount):
+		'''
+			PURPOSE:	Count the frequency of a given color in this palette's list of Colors
+			INPUT:  	wheelColorToCount - A valid color to count in this color palette
+			OUTPUT:  	The number of matching colors in this palette
+		'''
+		### INPUT VALIDATION ###
+		if not isinstance(wheelColorToCount, str):
+			raise TypeError("Wheel color is not a string")
+		elif wheelColorToCount not in self.validColors:
+			raise ValueError("Invalid wheel color")
+
+		### LOCAL VARIABLES ###
+		retVal = 0
+
+		### COUNT THE COLORS ###
+		for swatch in self.listOfColors:
+			if swatch.wheelColor == wheelColorToCount:
+				retVal += 1
+
+		return retVal
+
+
+	def get_color(self, colorToMatch = None):
+		### INPUT VALIDATION ###
+		if not isinstance(colorToMatch, Color) and colorToMatch is not None:
+			print("get_color():\tcolorToMatch is of type {}\n".format(colorToMatch))  # DEBUGGING
+			raise TypeError("Color to match is not of type Color or NoneType")
+		elif colorToMatch is not None:
+			if colorToMatch.colorType != "Primary":
+				raise ValueError("This function does not match non-Primary colors")
+		elif self.scheme not in self.implementedSchemes:
+			# print("get_color():\tcolorToMatch is of type {}\n".format(colorToMatch))  # DEBUGGING
+			print("get_color():\tself.scheme is {}\n".format(self.scheme))  # DEBUGGING
+			raise RuntimeError("This perfectly valid color scheme has not yet been implemented")
+
+		### LOCAL VARIABLES ###
+		retVal = None
+		randColor = None
+		primaryColorNum = 0
+		secondaryColorNum = 0
+		weaponColorNum = 0
+
+		### CALL CORRESPONDING METHOD ###
+		if self.scheme == "Monochromatic - Primary":
+			# Get Primary Armor Color
+			randColor = self.get_mono_primary(colorToMatch)
+		############# IMPLEMENT MORE COLOR SCHEMES HERE #############
+		else:
+			raise RuntimeError("How did we get here?!")
+
+		### EXTRACT THE COLOR NUMBER ###
+		retVal = randColor.num
+
+		return retVal
+
+
+	def get_mono_primary(self, colorToMatch = None):
+		'''
+			PURPOSE:	Get a primary color from the list of Colors in this palette
+			INPUT:		colorToMatch - Color class to match against
+			OUTPUT:		Color class of Type "Primary" that matches colorToMatch
+			NOTE:
+						If colorToMatch is None, will randomly select a primary wheelColor and then
+							randomly select a Color from the palette's list to match that wheelColor
+						If colorToMatch is a Color, will determine the Color's wheelColor and randomly
+							select a Color from the palette's list to match that wheelColor
+		'''
+		### LOCAL VARIABLES ###
+		retVal = None  			# Function's return value of type Color
+		matchThisColor = None  	# Primary color to match
+		numColors = 0  			# Holds of the number of given primary color in the list
+		randNum = 0				# Holds a randomized value
+
+		### GET PRIMARY COLOR ###
+		# print("Color To Match is of type:\t{}\n".format(colorToMatch))  # DEBUGGING
+		# 1. Determine primary color
+		if colorToMatch is None:  # This is the starting color
+			matchThisColor = self.listOfPrimaryColors[randint(0, self.listOfPrimaryColors.__len__() - 1)]
+		else:
+			matchThisColor = colorToMatch.wheelColor  # This is the color to match
+		# 2. Count the colors in the list
+		numColors = self.count_colors(matchThisColor)
+		if numColors <= 0:
+			raise ValueError("Could not find any {} colors".format(matchThisColor))
+		# 3. Randomly choose a color
+		randNum = randint(1, numColors)
+		# 4. Find the randNum'th Color in this palette's list
+		for swatch in self.listOfColors:
+			if swatch.wheelColor == matchThisColor:
+				randNum -= 1
+				if randNum == 0:
+					retVal = swatch
+					break
+
+		return retVal
+
+
+class MainArmorPalette(ColorPalette):
+	'This class can randomly choose main armor colors from a collection based on established color schemes'
+
+	def __init__(self, scheme):
+		### INPUT VALIDATION ###
+		if not isinstance(scheme, str):
+			raise TypeError("Color scheme is not an string")
+		elif scheme not in self.validSchemes:
+			raise ValueError("Invalid color scheme")
+		elif scheme not in self.implementedSchemes:
+			raise RuntimeError("This valid color scheme has not yet been implemented")
+
+		### INITIALIZAITON ###
+		self.scheme = scheme
+		self.listOfColors.append(Color(0, 72, 59, 20))
+		self.listOfColors.append(Color(1, 118, 41, 24))
+		self.listOfColors.append(Color(2, 32, 45, 26))
+		self.listOfColors.append(Color(3, 33, 57, 58))
+		self.listOfColors.append(Color(4, 31, 47, 84))
+		self.listOfColors.append(Color(5, 206, 20, 14))
+		self.listOfColors.append(Color(6, 194, 22, 31))
+		self.listOfColors.append(Color(7, 0, 100, 40))
+		self.listOfColors.append(Color(8, 0, 100, 23))
+		self.listOfColors.append(Color(9, 357, 90, 24))
+
+		self.listOfColors.append(Color(10, 359, 83, 27))
+		self.listOfColors.append(Color(11, 358, 77, 29))
+		self.listOfColors.append(Color(12, 357, 70, 30))
+		self.listOfColors.append(Color(13, 356, 60, 31))
+		self.listOfColors.append(Color(14, 21, 93, 90))
+		self.listOfColors.append(Color(15, 17, 94, 58))
+		self.listOfColors.append(Color(16, 19, 89, 59))
+		self.listOfColors.append(Color(17, 23, 84, 60))
+		self.listOfColors.append(Color(18, 25, 75, 62))
+		self.listOfColors.append(Color(19, 26, 62, 62))
+
+		self.listOfColors.append(Color(20, 28, 46, 63))
+		self.listOfColors.append(Color(21, 43, 96, 100))
+		self.listOfColors.append(Color(22, 35, 97, 73))
+		self.listOfColors.append(Color(23, 37, 91, 71))
+		self.listOfColors.append(Color(24, 39, 87, 71))
+		self.listOfColors.append(Color(25, 42, 79, 68))
+		self.listOfColors.append(Color(26, 45, 67, 67))
+		self.listOfColors.append(Color(27, 44, 52, 68))
+		self.listOfColors.append(Color(28, 78, 62, 22))
+		self.listOfColors.append(Color(29, 68, 55, 11))
+
+		self.listOfColors.append(Color(30, 70, 50, 14))
+		self.listOfColors.append(Color(31, 69, 43, 17))
+		self.listOfColors.append(Color(32, 72, 36, 22))
+		self.listOfColors.append(Color(33, 79, 24, 26))
+		self.listOfColors.append(Color(34, 82, 14, 31))
+		self.listOfColors.append(Color(35, 222, 90, 34))
+		self.listOfColors.append(Color(36, 223, 71, 16))
+		self.listOfColors.append(Color(37, 223, 71, 20))
+		self.listOfColors.append(Color(38, 220, 74, 24))
+		self.listOfColors.append(Color(39, 217, 75, 28))
+
+		self.listOfColors.append(Color(40, 213, 72, 31))
+		self.listOfColors.append(Color(41, 211, 67, 33))
+		self.listOfColors.append(Color(42, 256, 75, 31))
+		self.listOfColors.append(Color(43, 268, 67, 15))
+		self.listOfColors.append(Color(44, 262, 61, 19))
+		self.listOfColors.append(Color(45, 259, 60, 24))
+		self.listOfColors.append(Color(46, 257, 53, 29))
+		self.listOfColors.append(Color(47, 256, 46, 36))
+		self.listOfColors.append(Color(48, 250, 39, 43))
+		self.listOfColors.append(Color(49, 25, 68, 22))
+
+		self.listOfColors.append(Color(50, 24, 63, 13))
+		self.listOfColors.append(Color(51, 26, 57, 16))
+		self.listOfColors.append(Color(52, 24, 53, 18))
+		self.listOfColors.append(Color(53, 22, 42, 22))
+		self.listOfColors.append(Color(54, 20, 31, 26))
+		self.listOfColors.append(Color(55, 19, 21, 31))
+		self.listOfColors.append(Color(56, 200, 18, 13))
+		self.listOfColors.append(Color(57, 195, 18, 17))
+		self.listOfColors.append(Color(58, 206, 13, 22))
+		self.listOfColors.append(Color(59, 210, 12, 27))
+
+		self.listOfColors.append(Color(60, 206, 9, 32))
+		self.listOfColors.append(Color(61, 192, 5, 37))
+		self.listOfColors.append(Color(62, 204, 4, 44))
+		self.listOfColors.append(Color(63, 240, 3, 45))
+		self.listOfColors.append(Color(64, 240, 3, 54))
+		self.listOfColors.append(Color(65, 240, 1, 60))
+		self.listOfColors.append(Color(66, 0, 0, 68))
+		self.listOfColors.append(Color(67, 240, 2, 75))
+		self.listOfColors.append(Color(68, 0, 0, 81))
+		self.listOfColors.append(Color(69, 0, 0, 88))
+		
+		self.listOfColors.append(Color(70, 356, 100, 63))
+		self.listOfColors.append(Color(71, 9, 100, 35))
+		self.listOfColors.append(Color(72, 6, 100, 25))
+		self.listOfColors.append(Color(73, 15, 100, 100))
+		self.listOfColors.append(Color(74, 15, 100, 84))
+		self.listOfColors.append(Color(75, 19, 100, 48))
+		self.listOfColors.append(Color(76, 60, 100, 100))
+		self.listOfColors.append(Color(77, 51, 100, 100))
+		self.listOfColors.append(Color(78, 43, 100, 85))
+		self.listOfColors.append(Color(79, 114, 86, 72))
+
+		self.listOfColors.append(Color(80, 91, 100, 37))
+		self.listOfColors.append(Color(81, 100, 100, 20))
+		self.listOfColors.append(Color(82, 235, 100, 100))
+		self.listOfColors.append(Color(83, 237, 100, 100))
+		self.listOfColors.append(Color(84, 237, 100, 71))
+		self.listOfColors.append(Color(85, 261, 87, 100))
+		self.listOfColors.append(Color(86, 256, 100, 65))
+		self.listOfColors.append(Color(87, 259, 100, 35))
+		self.listOfColors.append(Color(88, 300, 66, 100))
+		self.listOfColors.append(Color(89, 321, 100, 100))
+
+		self.listOfColors.append(Color(90, 333, 100, 88))
+		self.listOfColors.append(Color(91, 0, 0, 100))
+		self.listOfColors.append(Color(92, 0, 0, 100))
+		self.listOfColors.append(Color(93, 0, 0, 86))
+		self.listOfColors.append(Color(94, 207, 100, 4))
+		self.listOfColors.append(Color(95, 204, 24, 8))
+		self.listOfColors.append(Color(96, 210, 17, 9))
+		self.listOfColors.append(Color(97, 177, 43, 21))
 
 
 
