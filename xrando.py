215703787256772eb782fc17e2be4f14ecffed91
hark130 <hark130@yahoo.com>
2017-08-22 00:28:41 -0500

    COLOR: Tweaked rando_color_scheme() for testing

--- xrando.py
+++ xrando.py
@@ -1423,14 +1423,15 @@ def rando_color_scheme(armorStyle):
 
     ### LOCAL VARIABLES ###
     # tmpInt = randint(0, listOfColorSchemes.__len__() - 1)  # Uncomment this once more color schemes are implemented in ColorPalette
-    tmpInt = randint(0, 2)
+    # tmpInt = randint(0, 3)
     
     ### RANDOMIZE A COLOR SCHEME ###
     ############ IMPLEMENT ARMOR STYLE INFLUENCES... LATER ############
-    retVal = listOfColorSchemes[tmpInt]  # Uncomment this once more color schemes are implemented in ColorPalette
+    # retVal = listOfColorSchemes[tmpInt]  # Uncomment this once more color schemes are implemented in ColorPalette
     # retVal = listOfColorSchemes[0]  # Monochromatic - Primary Colors
     # retVal = listOfColorSchemes[1]  # Monochromatic - Secondary Colors
     # retVal = listOfColorSchemes[2]  # Monochromatic - Tertiary Colors
+    retVal = listOfColorSchemes[3]  # 2 Colors - Analogous
     # print("Rando Color Scheme returns:\t{}\n".format(retVal))  # DEBUGGING
 
     return retVal
