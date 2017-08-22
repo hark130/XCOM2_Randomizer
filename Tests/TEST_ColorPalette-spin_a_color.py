df934d3ebb7dc7d07410b883741315e67ed41582
hark130 <hark130@yahoo.com>
2017-08-22 16:35:38 -0500

    COLOR: Finished writing Error tests
    All of the ColorPalette.spin_a_color() unit tests have been written.  Next, implement "offset 0" functionality. -Hark

--- Tests/TEST_ColorPalette-spin_a_color.py
+++ Tests/TEST_ColorPalette-spin_a_color.py
@@ -3,8 +3,9 @@
 # from .. import Harklepalette
 # import ..Harklepalette
 
-import unittest
+from random import randint
 import os, sys
+import unittest
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from Harklepalette import ColorPalette
 
@@ -114,19 +115,110 @@ class ErrorTests(SpinTest):
 
 
 	def test_01_Starting_Color_Non_String(self):
-		return
+		### LOCAL VARIABLES ###
+		tmpRetVal = None  	# Temporarily holds return value from spin_a_color
+		# startingColor parameter for this test
+		inputColors = [ \
+			None, {1:"One"}, [2, "Two"], \
+			(3, "Three"), int(4), float(5.00001), \
+			self.palette, True, False, \
+		]
+		staticOffset = 0	# Offset parameter for this test
+		skipGrey = True 	# skipGreyscale parameter for this test
+
+		### RUN THE TESTS ###
+		for inputColor in inputColors:
+			try:
+				tmpRetVal = self.palette.spin_a_color(inputColor, staticOffset, skipGrey)
+			except TypeError as err:
+				self.assertTrue(err.args[0] == "Starting color is not a string")
+			except Exception as err:
+				self.fail("Raised the wrong exception")
+			else:
+				self.fail("Did not raise an exception")
 
 
 	def test_02_Starting_Color_Invalid(self):
-		return
+		### LOCAL VARIABLES ###
+		tmpRetVal = None  	# Temporarily holds return value from spin_a_color
+		# startingColor parameter for this test
+		inputColors = [ \
+			"No", "Bad", "Invalid", \
+			"Colors are nice", "Some color", "color scheme", \
+			"ColorPalette", "Color", "Invalid starting color", \
+		]
+		for validColor in self.palette.validColors:
+			# UPPER CASE
+			inputColors.append(validColor.upper())
+			# lower case
+			inputColors.append(validColor.lower())
+			# Slice off index[0]
+			inputColors.append(validColor[1:])
+			# Slice off index[n]
+			inputColors.append(validColor[:validColor.__len__() - 2])
+
+		staticOffset = 0	# Offset parameter for this test
+		skipGrey = True 	# skipGreyscale parameter for this test
+
+		### RUN THE TESTS ###
+		for inputColor in inputColors:
+			try:
+				tmpRetVal = self.palette.spin_a_color(inputColor, staticOffset, skipGrey)
+			except ValueError as err:
+				self.assertTrue(err.args[0] == "Invalid starting color")
+			except Exception as err:
+				self.fail("Raised the wrong exception")
+			else:
+				self.fail("Did not raise an exception")
 
 
 	def test_03_Offset_Non_Integer(self):
-		return
+		### LOCAL VARIABLES ###
+		tmpRetVal = None  	# Temporarily holds return value from spin_a_color
+		# startingColor parameter for this test
+		inputColor = self.palette.validColors[randint(0, self.palette.validColors.__len__() - 1)]
+		staticOffsets = [ \
+			None, {1:"One"}, [2, "Two"], \
+			(3, "Three"), "Four", float(5.00001), \
+			self.palette, \
+		]
+		skipGrey = True 	# skipGreyscale parameter for this test
+
+		### RUN THE TESTS ###
+		for staticOffset in staticOffsets:
+			try:
+				tmpRetVal = self.palette.spin_a_color(inputColor, staticOffset, skipGrey)
+			except TypeError as err:
+				self.assertTrue(err.args[0] == "Offset is not an integer")
+			except Exception as err:
+				self.fail("Raised the wrong exception")
+			else:
+				self.fail("Did not raise an exception")
 
 
 	def test_04_Skip_Greyscale_Non_Bool(self):
-		return
+		### LOCAL VARIABLES ###
+		tmpRetVal = None  	# Temporarily holds return value from spin_a_color
+		# startingColor parameter for this test
+		inputColor = self.palette.validColors[randint(0, self.palette.validColors.__len__() - 1)]
+		staticOffset = 0	# Offset parameter for this test
+		# skipGreyscale parameter for this test
+		skipGreyInputs = [ \
+			None, {1:"One"}, [2, "Two"], \
+			(3, "Three"), int(4), float(5.00001), \
+			"Six", self.palette, \
+		]
+
+		### RUN THE TESTS ###
+		for skipGrey in skipGreyInputs:
+			try:
+				tmpRetVal = self.palette.spin_a_color(inputColor, staticOffset, skipGrey)
+			except TypeError as err:
+				self.assertTrue(err.args[0] == "Skip Greyscale is not a bool")
+			except Exception as err:
+				self.fail("Raised the wrong exception")
+			else:
+				self.fail("Did not raise an exception")
 
 
 class BoundaryTests(SpinTest):
